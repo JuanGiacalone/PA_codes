1bcbdb262e96486828a2e20c81df3d4440009867
JuanGiacalone <juanpablogiacalone8@gmail.com>
2020-09-12 01:51:04 -0300

    init

---
+++ cd1/main.c
@@ -0,0 +1,17 @@
+// OpenMP header 
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+        int nthreads, tid;
+
+        // Begin of parallel region 
+        #pragma omp parallel                   
+        {
+        printf("Hello World... from thread = %d\n",
+                omp_get_thread_num());
+        }
+}
+// testeando OpenMP
